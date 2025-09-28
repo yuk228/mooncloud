ad9ff4699cbe00ca48bfd2309ee500f2cb71d249
Yuki <moyaiscan@tutamail.com>
2025-09-28 15:44:42 +0900

    mysql dbを追加しました

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Hello World from MoonCloud"}
 
 
 @app.get("/items/{item_id}")
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div>
-      <p>Hello Worldd</p>
-    </div>
+    <main>
+      <h1>Hello World</h1>
+    </main>
   );
 }
